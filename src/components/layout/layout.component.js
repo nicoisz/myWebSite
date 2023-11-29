bc780da1947f45a4fe78b6a30db618e28dcb2f05
nicoisz <nicolasignacio.sz@gmail.com>
2023-11-28 23:18:22 -0300

    changing stufs

--- src/components/layout/layout.component.js
+++ src/components/layout/layout.component.js
@@ -1,12 +1,24 @@
+import { Outlet } from 'react-router-dom';
 import SidebarComponent from '../sidebar/sidebar.component'
 import './layout.styles.scss'
 
 const LayoutPage = () => {
     return (
-        <div>
-            <SidebarComponent />
+        <div className="App">
+          <SidebarComponent />
+          <div className="page">
+            <span className="tags top-tags">&lt;body&gt;</span>
+    
+            <Outlet />
+            <span className="tags bottom-tags">
+              &lt;/body&gt;
+              <br />
+              <span className="bottom-tag-html">&lt;/html&gt;</span>
+            </span>
+          </div>
         </div>
-    )
+      )
+    
 };
 
 export default LayoutPage;
