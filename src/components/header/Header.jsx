542b10138f6ac81512b81d4de4670dee537f04d4
nicoisz <nicolasignacio.sz@gmail.com>
2024-04-05 11:16:24 -0600

    working en header menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import "./header.css"
 
-const Header = () => {
+const header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header className="header">
+      <nav className='nav container'>
+        <a href='index.html' className='nav__logo'>nico</a>
+
+        <div className='nav__menu'>
+          <ul className='nav__list grid'>
+            <li className='nav__item'>
+              <a href='#home' className='nav__link'>
+                <i class="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href='#about' className='nav__link'>
+                <i className='uil uil-user nav__icon'></i> About
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href='#skills' className='nav__link'>
+                <i className='uil uil-file-alt nav__icon'></i> Skills
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href='#services' className='nav__link'>
+                <i className='uil uil-briefcase-alt nav__icon'></i> Services
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href='#portfolio' className='nav__link'>
+                <i className='uil uil-scenery nav__icon'></i> Portfolio
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href='#contact' className='nav__link'>
+                <i className='uil uil-message nav__icon'></i> Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   )
 }
 
-export default Header
+export default header
