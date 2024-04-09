b9b6528cc3a8d84a1510e5e027f5334b6c39211c
nicoisz <nicolasignacio.sz@gmail.com>
2024-04-08 21:26:05 -0600

    creacion de pagina muy pequeña

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import './home.css'
 import Social from './Social'
 import Data from './Data'
+import NoSpace from './noSpace/NoSpace'
 
 const Home = () => {
   return (
     <div>
+      <div className='squeezed'>
+        <NoSpace/>
+      </div>
+      <div className='main'></div>
       <section className='section home' id='home'>
         <div className='home__container container grid'>
           <div className='home__content grid'>
---
+++ src/components/home/noSpace/NoSpace.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './noSpace.css'
+
+const NoSpace = () => {
+  return (
+    <div>
+        <div class="container">
+
+
+            <div class="section">
+                <p class="text">There is no space here</p>
+                <img src="http://salehriaz.com/404Page/img/earth.svg"  alt="" class="earth"/>
+                <img src="/rocket.png" alt="" class="rocket"/>
+                <img src="http://salehriaz.com/404Page/img/moon.svg" alt="" class="moon"/>
+                <img src="http://salehriaz.com/404Page/img/astronaut.svg"  alt="" class="ast"/>
+            </div>
+        
+            <section class="wrapper">
+                <div id="stars1"></div>
+                <div id="stars2"></div>
+                <div id="stars3"></div>
+              </section>
+              
+         </div>
+    </div>
+  )
+}
+
+export default NoSpace
