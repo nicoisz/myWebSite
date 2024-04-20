4264e8c106865ede3ee43b206d3dd16f421ae7df
nicoisz <nicolasignacio.sz@gmail.com>
2024-04-20 10:47:03 -0600

    more services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,7 +3,51 @@ import './services.css'
 
 function Services() {
   return (
-    <div>Services</div>
+      <section className="services section" id="services">
+          <h2 className="section__title">Services</h2>
+          <span className="section__subtitle">What i offer</span>
+
+          <div className="services__container container grid">
+              <div className="services__content">
+                  <div>
+                      <i className="uil uil services__icon"></i>
+                      <h3 className="services__title"></h3>
+                      <span className="services__button">View More
+                          <i className="uil uil-arrow-right services__button-icon"></i>
+                      </span>
+
+                      <div className="services__modal">
+                          <div className="services__modal-content">
+                              <i className="uil uil-times services__modal-close"></i>
+                              <h3 className="services__modal-title"></h3>
+                              <p className="services__modal-description">Service with more than 6 years of experience.
+                                Providing quality work to clients and companies.
+                              </p>
+
+                              <ul className="services__modal-services grid">
+                                  <li className="services__modal-service">
+                                      <i className="uil uil-check-circle services__modal-icon"></i>
+                                      <p className="services__modal-info">I Develop the user interface.</p>
+                                  </li>
+                                  <li className="services__modal-service">
+                                      <i className="uil uil-check-circle services__modal-icon"></i>
+                                      <p className="services__modal-info">Web page development.</p>
+                                  </li>
+                                  <li className="services__modal-service">
+                                      <i className="uil uil-check-circle services__modal-icon"></i>
+                                      <p className="services__modal-info">I create mobile apps.</p>
+                                  </li>
+                                  <li className="services__modal-service">
+                                  <i className="uil uil-check-circle services__modal-icon"></i>
+                                  <p className="services__modal-info">I design you brand.</p>
+                              </li>
+                              </ul>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
   )
 }
 
