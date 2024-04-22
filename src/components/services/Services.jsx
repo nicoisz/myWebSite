921ddc41dc1a66e6a3ff41c4f3b5df46e975979d
nicoisz <nicolasignacio.sz@gmail.com>
2024-04-22 15:54:44 -0600

    services container

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -10,16 +10,94 @@ function Services() {
           <div className="services__container container grid">
               <div className="services__content">
                   <div>
-                      <i className="uil uil services__icon"></i>
-                      <h3 className="services__title"></h3>
-                      <span className="services__button">View More
+                      <i className="uil uil-web-grid services__icon"></i>
+                      <h3 className="services__title">Product <br/>Designer</h3>
+                      <span className="services__button">View More {""}
+                          <i className="uil uil-arrow-right services__button-icon"></i>
+                      </span>
+
+                      <div className="services__modal">
+                          <div className="services__modal-content">
+                              <i className="uil uil-times services__modal-close">Product Designer</i>
+                              <h3 className="services__modal-title"></h3>
+                              <p className="services__modal-description">Service with more than 6 years of experience.
+                                Providing quality work to clients and companies.
+                              </p>
+
+                              <ul className="services__modal-services grid">
+                                  <li className="services__modal-service">
+                                      <i className="uil uil-check-circle services__modal-icon"></i>
+                                      <p className="services__modal-info">I Develop the user interface.</p>
+                                  </li>
+                                  <li className="services__modal-service">
+                                      <i className="uil uil-check-circle services__modal-icon"></i>
+                                      <p className="services__modal-info">Web page development.</p>
+                                  </li>
+                                  <li className="services__modal-service">
+                                      <i className="uil uil-check-circle services__modal-icon"></i>
+                                      <p className="services__modal-info">I create mobile apps.</p>
+                                  </li>
+                                  <li className="services__modal-service">
+                                  <i className="uil uil-check-circle services__modal-icon"></i>
+                                  <p className="services__modal-info">I design you brand.</p>
+                              </li>
+                              </ul>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="services__content">
+                  <div>
+                      <i className="uil uil-arrow services__icon"></i>
+                      <h3 className="services__title">UI / UX <br/>Designer</h3>
+                      <span className="services__button">View More {""}
                           <i className="uil uil-arrow-right services__button-icon"></i>
                       </span>
 
                       <div className="services__modal">
                           <div className="services__modal-content">
                               <i className="uil uil-times services__modal-close"></i>
-                              <h3 className="services__modal-title"></h3>
+                              <h3 className="services__modal-title">UI / UX Designer</h3>
+                              <p className="services__modal-description">Service with more than 6 years of experience.
+                                Providing quality work to clients and companies.
+                              </p>
+
+                              <ul className="services__modal-services grid">
+                                  <li className="services__modal-service">
+                                      <i className="uil uil-check-circle services__modal-icon"></i>
+                                      <p className="services__modal-info">I Develop the user interface.</p>
+                                  </li>
+                                  <li className="services__modal-service">
+                                      <i className="uil uil-check-circle services__modal-icon"></i>
+                                      <p className="services__modal-info">Web page development.</p>
+                                  </li>
+                                  <li className="services__modal-service">
+                                      <i className="uil uil-check-circle services__modal-icon"></i>
+                                      <p className="services__modal-info">I create mobile apps.</p>
+                                  </li>
+                                  <li className="services__modal-service">
+                                  <i className="uil uil-check-circle services__modal-icon"></i>
+                                  <p className="services__modal-info">I design you brand.</p>
+                              </li>
+                              </ul>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="services__content">
+                  <div>
+                      <i className="uil uil-edit services__icon"></i>
+                      <h3 className="services__title">Visual <br/>Designer</h3>
+                      <span className="services__button">View More {""}
+                          <i className="uil uil-arrow-right services__button-icon"></i>
+                      </span>
+
+                      <div className="services__modal">
+                          <div className="services__modal-content">
+                              <i className="uil uil-times services__modal-close"></i>
+                              <h3 className="services__modal-title">Visual Designer</h3>
                               <p className="services__modal-description">Service with more than 6 years of experience.
                                 Providing quality work to clients and companies.
                               </p>
