3448b3beb212ba16d4c99d24117e99a05e0cddb4
nicoisz <nicolasignacio.sz@gmail.com>
2024-05-14 12:41:04 -0600

    integration of email js

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useRef }  from 'react';
 import './contact.css';
 import { ReactComponent as Send } from "../../assets/send.svg";
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_7llin5s', 'template_0ldwvrk', form.current, 
+    '_LeUJEz55mAVNq7J6',
+      )
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+    
   return (
       <section className="contact section" id="contact">
           <h2 className="section__title">Get in touch</h2>
@@ -40,7 +61,8 @@ const Contact = () => {
               </div>
               <div className="contact__content">
               <h3 className="contact__title">Write me your projects</h3>
-              <form action="" className="contact__form">
+              <form action="" className="contact__form" ref={form} onSubmit={sendEmail}>
+
                   <div className="contact__form-div">
                       <label htmlFor="" className="contact__form-tag">Name</label>
                       <input type="text" className="contact__form-input" name='name' placeholder='Insert your name'/>
@@ -49,7 +71,7 @@ const Contact = () => {
                       <label htmlFor="" className="contact__form-tag">Email</label>
                       <input type="email" className="contact__form-input" name='email' placeholder='Insert your email'/>
                   </div>
-                  <div className="contact__form-div">
+                  <div className="contact__form-div contact__form-area">
                       <label htmlFor="" className="contact__form-tag">Project</label>
                       <textarea name='project' id='' cols='30' rows='10' className='contact__form-input' placeholder='Tell me your project'/>
                   </div>
